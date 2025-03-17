63b610fb3cdd63d385a3990228cb3e2d109ca7c6
Ivan Franco <ifrancoh@ucsc.edu>
2025-03-17 14:20:41 -0700

    implemented vertical movement on y-axis in PlayerMovementScript

--- Assets/GameJam/Scripts/PlayerMovement.cs
+++ Assets/GameJam/Scripts/PlayerMovement.cs
@@ -5,27 +5,31 @@ public class PlayerMovement : MonoBehaviour
     [Tooltip("Controls how fast the player moves")]
     [SerializeField] private float playerSpeed = 5.0f;
 
-    //private Rigidbody playerRigidbody;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        //playerRigidbody = GetComponent<Rigidbody>();
-    }
-
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        // Horizontal/x-axis 
         float horizontalMoveTest = Input.GetAxis("Horizontal");
-        //Vector3 horizontalForceValue = new Vector3(horizontalMoveTest, 0f, 0f);
         Vector3 currentPlayerPosition = transform.position;
         Vector3 newPlayerPosition = currentPlayerPosition;
+
+        // Vertical/y-axis 
+        float verticalMoveTest = Input.GetAxis("Vertical");
 
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
         {
             Debug.Log(horizontalMoveTest);
 
             newPlayerPosition += new Vector3(horizontalMoveTest, 0f, 0f) * playerSpeed * Time.deltaTime;
+
+            transform.position = newPlayerPosition;
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.W))
+        {
+            Debug.Log(verticalMoveTest);
+
+            newPlayerPosition += new Vector3(0f, verticalMoveTest, 0f) * playerSpeed * Time.deltaTime;
 
             transform.position = newPlayerPosition;
         }
