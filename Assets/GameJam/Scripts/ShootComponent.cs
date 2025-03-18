3a7929fb564e20303a62a68fed6684f655e47230
Ivan Franco <ifrancoh@ucsc.edu>
2025-03-17 17:03:19 -0700

    apply force to spawn bullet prefab

--- Assets/GameJam/Scripts/ShootComponent.cs
+++ Assets/GameJam/Scripts/ShootComponent.cs
@@ -8,6 +8,9 @@ public class ShootComponent : MonoBehaviour
     [Tooltip("The distance in front of player to spawn the Bullet Prefab at")]
     [SerializeField] private float spawnBulletDistance = 1.75f;
 
+    [Tooltip("The force to apply on the y-axis to the Bullet Prefab")]
+    [SerializeField] private float bulletVerticalForce = 5.0f;
+
     // Update is called once per frame
     void Update()
     {
@@ -16,7 +19,11 @@ public class ShootComponent : MonoBehaviour
         Vector3 spawnBulletPosition = new Vector3(transform.position.x, transform.position.y + spawnBulletDistance, transform.position.z);
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(bulletPrefab, spawnBulletPosition, Quaternion.identity);
+            GameObject instantiatedBulletPrefab = Instantiate(bulletPrefab, spawnBulletPosition, Quaternion.identity);
+            if (instantiatedBulletPrefab.TryGetComponent<Rigidbody>(out Rigidbody rbBulletPrefab))
+            {
+                rbBulletPrefab.AddForce(new Vector3(0f, bulletVerticalForce, 0f), ForceMode.Impulse);
+            }
         }
     }
 }
