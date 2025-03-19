5f4d19c98fbf904f7ada47546a5162f06ca0cca2
Ivan Franco <ifrancoh@ucsc.edu>
2025-03-18 17:01:16 -0700

    Bullet prefab now get deleted by collision or by existing in the world after a specified amount of time

--- Assets/GameJam/Scripts/BulletController.cs
+++ Assets/GameJam/Scripts/BulletController.cs
@@ -2,9 +2,38 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
+    [Tooltip("Time, in seconds, to destroy Bullet Prefab after spawn")]
+    [SerializeField] private float timeToDestroyBullet = 2.0f;
+
+    private bool isBulletVisible = false;
+
+    private void OnBecameVisible()
+    {
+        isBulletVisible = true;
+    }
+
+    private void Update()
+    {
+        if (!isBulletVisible)
+        {
+            return;
+        }
+        else
+        {
+            Debug.Log(timeToDestroyBullet);
+            Invoke(nameof(TrackVisibility), timeToDestroyBullet);
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Collided");
+        Destroy(this.gameObject);
+    }
+
+    private void TrackVisibility()
+    {
+        Debug.Log($"{timeToDestroyBullet}");
         Destroy(this.gameObject);
     }
 }
--- Assets/GameJam/Scripts/ShootComponent.cs
+++ Assets/GameJam/Scripts/ShootComponent.cs
@@ -12,27 +12,13 @@ public class ShootComponent : MonoBehaviour
     [Tooltip("The force to apply on the y-axis to the Bullet Prefab")]
     [SerializeField] private float bulletVerticalForce = 5.0f;
 
-    [Tooltip("Time, in seconds, to destroy Bullet Prefab after spawn")]
-    [SerializeField] private float timeToDestroyBullet = 0.5f;
-    private float internalTimeToDestroyBullet = 0.0f;
-
-    //private List<GameObject> listOfSpawnBulletPrefabs;
-
-    private void Start()
-    {
-        //listOfSpawnBulletPrefabs = new List<GameObject>();
-    }
-
     // Update is called once per frame
     private void Update()
     {
-        // If player presses SPACE
-        // ... instantiate/spawn a bullet in front of player
         Vector3 spawnBulletPosition = new Vector3(transform.position.x, transform.position.y + spawnBulletDistance, transform.position.z);
         if (Input.GetKeyDown(KeyCode.Space))
         {
             GameObject instantiatedBulletPrefab = Instantiate(bulletPrefab, spawnBulletPosition, Quaternion.identity);
-            //listOfSpawnBulletPrefabs.Add(instantiatedBulletPrefab);
             if (instantiatedBulletPrefab.TryGetComponent<Rigidbody>(out Rigidbody rbBulletPrefab))
             {
                 rbBulletPrefab.AddForce(new Vector3(0f, bulletVerticalForce, 0f), ForceMode.Impulse);
